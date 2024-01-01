e7c9141c9f4be82095ad555b6ff7aadc1082a8f0
eyusufyildiz <eyusufyildiz@gmail.com>
2023-12-31 21:44:05 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -2,5 +2,5 @@ import streamlit as st
 from python_wireguard import Key
 
 private, public = Key.key_pair()
-st.write(f"Private-key:" {private})
-st.write(f"Public-key:" {private})
+st.write(f"Private-key: {private}")
+st.write(f"Public-key: {private}")
