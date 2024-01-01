c07d03c26ca630527c7818b349616766edb15af9
eyusufyildiz <eyusufyildiz@gmail.com>
2023-12-31 22:07:00 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from python_wireguard import Key
+import subprocess
 
 private, public = Key.key_pair()
 st.write(f"Private-key: {private}")
 st.write(f"Public-key: {public}")
+
+
+cmds=["df -h", "cat /etc/os-release", "ifconfig" ]
+
+for cmd in cmds:
+    res = subprocess.check_output("wcat ", shell=True).decode("utf-8").strip()
+    st.code(res)
