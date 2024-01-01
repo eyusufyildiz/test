be266cedbc09f0f88c372991e617ba8f710852a8
eyusufyildiz <eyusufyildiz@gmail.com>
2023-12-31 22:11:32 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -11,4 +11,5 @@ cmds=["df -h", "cat /etc/os-release", "sudo apt list --installed" ]
 
 for cmd in cmds:
     res = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
+    st.text(cmd)
     st.code(res)
