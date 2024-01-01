68dc9a23eb4a52d8f87baccf6f5cbba396d336e4
eyusufyildiz <eyusufyildiz@gmail.com>
2023-12-31 22:09:12 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -7,7 +7,7 @@ st.write(f"Private-key: {private}")
 st.write(f"Public-key: {public}")
 
 
-cmds=["df -h", "cat /etc/os-release", "ifconfig" ]
+cmds=["df -h", "cat /etc/os-release", "sudo apt install wireguard" ]
 
 for cmd in cmds:
     res = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
