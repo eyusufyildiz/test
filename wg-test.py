34a5d677a9bbe433e378a8e04bfed38d1bca7dc9
eyusufyildiz <eyusufyildiz@gmail.com>
2024-01-01 22:59:14 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -2,38 +2,42 @@ import streamlit as st
 from python_wireguard import Key
 import subprocess
 
-if st.button("Baloons"):
-      st.balloons()
+def showing():
+      if st.button("Baloons"):
+            st.balloons()
+      
+      if st.button("snow"):
+            st.snow()
+      
+      if st.button("toast"):
+            st.toast('Berkehan & Bilge')
 
-if st.button("snow"):
-      st.snow()
+def wg_keys():
+      private, public = Key.key_pair()
+      jData = {"wg-Private-key":private, "wg-Public-key":public}
+      st.json( jData )
 
-if st.button("toast"):
-      st.toast('Berkehan & Bilge')
-
-private, public = Key.key_pair()
-jData = {"wg-Private-key":private, "wg-Public-key":public}
-st.json( jData )
-
-
-cmds=["df -h", 
-      "cat /etc/os-release", 
-      "curl https://api.ipify.org ", 
-      "curl ifconfig.me/all.json",
-      "curl https://vpnstar.streamlit.app/",
-      "pip list",
-      "cat /proc/cpuinfo | grep proc",
-      "hostname",
-      "free -m"
-      ]
-
-for cmd in cmds:
-    st.markdown(f"* {cmd}")
-    try:
-          res = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
-          st.code(res)
-    except:
-          pass
+def run_os_commands():
+      cmds=["df -h", 
+            "cat /etc/os-release", 
+            "curl https://api.ipify.org ", 
+            "curl ifconfig.me/all.json",
+            "curl https://vpnstar.streamlit.app/",
+            "pip list",
+            "cat /proc/cpuinfo | grep proc",
+            "hostname",
+            "free -m"
+            ]
+      
+      for cmd in cmds:
+          st.markdown(f"* {cmd}")
+          try:
+                res = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
+                st.code(res)
+          except:
+                pass
 
 
-
+showing()
+wg_keys()
+run_os_commands()
