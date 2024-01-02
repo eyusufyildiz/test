072d322a4be5165cc26dcdaaeb8a2c395456d398
eyusufyildiz <eyusufyildiz@gmail.com>
2024-01-02 11:19:08 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -8,7 +8,7 @@ def hide_streamlit():
       hide_streamlit_style = """
             <style>
                   #MainMenu {visibility: hidden;}
-                  footer {visibility: hidden;}
+                  #footer {visibility: hidden;}
             </style>
       """
       st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
