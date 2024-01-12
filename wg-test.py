b13d77df22fd37efcd157e018a673f4534b2c3b9
eyusufyildiz <eyusufyildiz@gmail.com>
2024-01-12 22:58:38 +0000

    updates

--- wg-test.py
+++ wg-test.py
@@ -23,7 +23,7 @@ def hide_streamlit():
 def collect():
    if st.button("Collect data:"):
       # Get the query parameters
-      qp = st.query_params()
+      qp = st.query_params
 
       # Display the query parameters
       st.write("Query Parameters:", qp)
