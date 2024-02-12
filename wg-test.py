c778d2cefcb3c9eb2fec3277418fbb955d771686
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-11 23:30:24 -0800

    Update wg-test.py

--- wg-test.py
+++ wg-test.py
@@ -10,19 +10,7 @@ def menu_exm():
 	import streamlit as st
 	from streamlit_option_menu import option_menu
 	
-	# 4. Manual item selection
-	if st.session_state.get('switch_button', False):
-	    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
-	    manual_select = st.session_state['menu_option']
-	else:
-	    manual_select = None
-	    
-	selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-	    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-	    orientation="horizontal", manual_select=manual_select, key='menu_4')
-	st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-	selected4
-	
+
 	# 5. Add on_change callback
 	def on_change(key):
 	    selection = st.session_state[key]
