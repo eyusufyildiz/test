d0844d01c73a0ea29804bbb29a4d983a749b3e54
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-11 23:56:21 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -2,19 +2,19 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # https://github.com/victoryhb/streamlit-option-menu
-def menu_exm():
+def menu_exm(idx):
 	# 5. Add on_change callback
 	def on_change(key):
 	    selection = st.session_state[key]
 	    st.write(f"Selection changed to {selection}")
 
-	idx=2
 	selected5 = option_menu("Welcome", ["Home", "Upload", "Tasks", 'Settings'],
 	                        icons=['house', 'cloud-upload', "list-task", 'gear'],
 	                        on_change=on_change, default_index=idx, key='Tasks', orientation="horizontal")
 	selected5
+	idx
 	
 
-menu_exm()
+menu_exm(2)
 
 st.write("----")
