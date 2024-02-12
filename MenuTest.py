7aec37080860c7dc6ccd86476a88009b8ff39230
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-11 23:59:41 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -14,7 +14,7 @@ def menu_exm(idx):
 	selected5
 	idx
 	
-with st.selectbox('Select', [0,1,2,3]):
-    menu_exm(2)
+selected = st.selectbox('Select', [0,1,2,3]):
+menu_exm(selected)
 
 st.write("----")
