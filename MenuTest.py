e8c16ba6ff14bd4a213324043954a38fce013007
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-12 00:44:40 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -4,7 +4,7 @@ from streamlit_option_menu import option_menu
 def menu(idx=0):
 	with st.sidebar:
 	    selected = option_menu("Menu", ["Home", 'Settings'], 
-	        icons=['house', 'gear'], default_index=idx)
+	        icons=['house', 'gear'], default_index=idx, key=idx)
 	
 	if selected == "Home":
 	    st.write("home is where the heart is")
