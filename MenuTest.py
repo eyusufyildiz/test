8640d08bdd73f287c61e2392af2f69d1ee956bb4
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-13 00:58:18 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -38,7 +38,7 @@ lat, lon = map()
 lat, lot = "{:.4f}".format(lat), "{:.4f}".format(lon)
 st.code([lat, lot])
 
-if geo_reverse(lat, lon) :
-    st.write("Addess;")
+if lat and lot and geo_reverse(lat, lon):
+    st.text("Addess:")
     tbl = pd.json_normalize( geo_reverse(lat, lon) )
     st.write( tbl )
