f5a0080984c238e20119c427630b6ed3e1994701
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-12 23:44:20 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -3,19 +3,13 @@ from streamlit_folium import st_folium
 import streamlit as st
 
 
-def get_pos(lat, lng):
-    return lat, lng
-
-
 m = fl.Map()
-
 m.add_child(fl.LatLngPopup())
-
 map = st_folium(m, height=350, width=700)
 
 data = None
 if map.get("last_clicked"):
-    data = get_pos(map["last_clicked"]["lat"], map["last_clicked"]["lng"])
+    data = [map["last_clicked"]["lat"], map["last_clicked"]["lng"] ]
 
 if data is not None:
     st.write(data) # Writes to the app
