3656eba21b23d6570e739aa6c55a3c326c608091
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-13 00:16:25 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -1,7 +1,9 @@
 import folium as fl
 from streamlit_folium import st_folium
 import streamlit as st
+import pandas as pd
 from geopy.geocoders import Nominatim
+
 
 def map():
     m = fl.Map()
@@ -15,25 +17,27 @@ def map():
         return data
 
 
-def geo_reverse(ldata):
-    st.write(data)
-    lat, lon = data[0],  data[1]
-    if lat and lon:
-        geolocator = Nominatim(user_agent="geoapiExercises")
-        geolocator = Nominatim(user_agent="geoapiIssNow")
-        location    = geolocator.reverse(str(lat) + ", " + str(lon))
-        location_en = geolocator.reverse(str(lat) + ", " + str(lon), language='en')
-        #location    = geolocator.reverse(f"{lat}, {lon}")
-        #location_en = geolocator.reverse(f"{lat}, {lon}", language='en')
-        
-        try:
-            address = location.raw['address']
-            address_en = location_en.raw['address']
-            st.write( address )
-            st.write( address_en )
-        except:
-            return None
+def geo_reverse(lat, lon ):
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    geolocator = Nominatim(user_agent="geoapiIssNow")
+    location    = geolocator.reverse(str(lat) + ", " + str(lon))
+    location_en = geolocator.reverse(str(lat) + ", " + str(lon), language='en')
+    #location    = geolocator.reverse(f"{lat}, {lon}")
+    #location_en = geolocator.reverse(f"{lat}, {lon}", language='en')
+    
+    try:
+        address = location.raw['address']
+        address_en = location_en.raw['address']
+        st.write( address )
+        st.write( address_en )
+    except:
+        return None
+
 
 data = map()
-if data:
-    geo_reverse(data )
+lat, lon=data[0], data[10
+
+if geo_reverse(lat, lon):
+    st.write("ISS is now on below address/place:")
+    tbl = pd.json_normalize( geo_reverse(lat, lon) )
+    st.write( tbl )
