7c41a820433cc465b7ca77e311da57de3339b9a0
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-12 23:07:15 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -1,30 +1,30 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import folium
 
 def menu(idx=0):
 	with st.sidebar:
 	    selected = option_menu("Menu", ["Home",  "Select on Map"], 
-	        icons=['house', 'gear'], default_index=idx, key=idx)
+		icons=['house', 'gear'], default_index=idx, key=idx)
 	
 	if selected == "Home":
 	    st.write("home is where the heart is")
 	else:
-		st.write("sSelect on Map")
-		import folium
-		
-		# Create a map
-		map = folium.map()
-		
-		# Add a marker to the map at the clicked location
-		marker = folium.map.ClickForMarker()
-		map.add_child(marker)
-		
-		# Get the latitude and longitude of the clicked location
-		latitude = marker.location[0]
-		longitude = marker.location[1]
-		
-		# Print the latitude and longitude
-		print(latitude, longitude)     
+	    st.title("Get Latitude and Longitude from Map")
+	
+	    # Create a map object using Folium
+	    m = folium.Map(location=[0, 0], zoom_start=2)
+	
+	    # Display the map using Streamlit
+	    folium_static(m)
+	
+	    # Get latitude and longitude coordinates on click   
+
+def get_coordinates(m):
+    # Function to get latitude and longitude coordinates on click
+    m.add_child(folium.LatLngPopup())
+
+
 
 sl = st.selectbox('Select', [0, 1])
 menu(sl)
