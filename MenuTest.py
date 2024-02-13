be2ce5fc45fc875d3ef535bbd07c3fcdf85c5b58
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-12 23:16:10 -0800

    Update MenuTest.py

--- MenuTest.py
+++ MenuTest.py
@@ -1,30 +1,26 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import folium
 from streamlit_folium import folium_static
 
 
-def menu(idx=0):
-	with st.sidebar:
-	    st.title("Get Latitude and Longitude from Map")
-	
-	    # Create a map object using Folium
-	    m = folium.Map(location=[0, 0], zoom_start=2)
-	
-	    # Display the map using Streamlit
-	    folium_static(m)
-	
-	    # Get latitude and longitude coordinates on click
-	    get_coordinates(m)
+def main():
+    st.title("Get Latitude and Longitude from Map")
 
-	def get_coordinates(m):
-	    # Function to get latitude and longitude coordinates on click
-	    lat_lon = folium.LatLngPopup()
-	    m.add_child(lat_lon)
-	    st.write("Latitude and Longitude Coordinates:")
-	    st.write(lat_lon)
+    # Create a map object using Folium
+    m = folium.Map(location=[0, 0], zoom_start=2)
 
+    # Display the map using Streamlit
+    folium_static(m)
 
+    # Get latitude and longitude coordinates on click
+    get_coordinates(m)
 
-sl = st.selectbox('Select', [0, 1])
-menu(sl)
+def get_coordinates(m):
+    # Function to get latitude and longitude coordinates on click
+    lat_lon = folium.LatLngPopup()
+    m.add_child(lat_lon)
+    st.write("Latitude and Longitude Coordinates:")
+    st.write(lat_lon)
+
+if __name__ == "__main__":
+    main()
