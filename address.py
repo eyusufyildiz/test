10c64e3c0632a10bd132eb1567747bf1d8526701
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-14 00:21:21 -0800

    Update address.py

--- address.py
+++ address.py
@@ -6,12 +6,12 @@ from geopy.geocoders import Nominatim
 
 
 def map():
-    data = None
     try:
         m = fl.Map()
         m.add_child(fl.LatLngPopup())
         map = st_folium(m, height=350, width=700)
-
+        data = None
+        
         if map.get("last_clicked"):
             data = ( map["last_clicked"]["lat"], map["last_clicked"]["lng"] )
             #st.write(data) # Writes to the app
