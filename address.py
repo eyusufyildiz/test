0dc39b0fd5b11092cf9700023b61f8b59a2a872f
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-13 22:59:03 -0800

    Update address.py

--- address.py
+++ address.py
@@ -45,7 +45,7 @@ def address():
             st.text("Addess:")
             tbl = pd.json_normalize( geo_reverse(lat, lon) )
             st.write( tbl )
-            st.code(tbl)
+            st.code(( geo_reverse(lat, lon) )
     except:
         return None
         
