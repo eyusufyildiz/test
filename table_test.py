cc08bac399c5d57dcf4f43d6a817a04d3b3bf0aa
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-20 08:41:03 +0000

    ii

--- table_test.py
+++ table_test.py
@@ -1,6 +1,25 @@
 import streamlit as st
-from st_aggrid import AgGrid
 import pandas as pd
+from st_aggrid import AgGrid
 
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
-AgGrid(df)
+df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+
+grid_options = {
+    "columnDefs": [
+        {
+            "headerName": "col1",
+            "field": "col1",
+            "editable": True,
+        },
+        {
+            "headerName": "col2",
+            "field": "col2",
+            "editable": False,
+        },
+    ],
+}
+
+grid_return = AgGrid(df, grid_options)
+new_df = grid_return["data"]
+
+st.write(new_df)
