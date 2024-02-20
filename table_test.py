aa9f85f7ed1f6456517a84d1cf3f4a9b3ba423b8
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-20 08:45:03 +0000

    ii

--- table_test.py
+++ table_test.py
@@ -2,24 +2,6 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid
 
-df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
-
-grid_options = {
-    "columnDefs": [
-        {
-            "headerName": "col1",
-            "field": "col1",
-            "editable": True,
-        },
-        {
-            "headerName": "col2",
-            "field": "col2",
-            "editable": True,
-        },
-    ],
-}
-
-grid_return = AgGrid(df, grid_options)
-new_df = grid_return["data"]
-
-st.write(new_df)
+df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
+grid_return = AgGrid(df, editable=True)
+new_df = grid_return['data']
