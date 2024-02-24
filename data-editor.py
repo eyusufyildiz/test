c4f11ed6c250e1f3ead021ac62a149c0c33458f4
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-24 22:29:56 +0000

    ...

--- data-editor.py
+++ data-editor.py
@@ -1,24 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame(
-    [
-       {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 5, "is_widget": False},
-       {"command": "st.time_input", "rating": 3, "is_widget": True},
-   ]
-)
+
+# Create a dataframe
+df = pd.DataFrame({'name': ['Alice', 'Bob', 'Carol'], 'age': [25, 30, 35]})
 
 # Define a function to be called when the data is changed
 def on_change(df):
     # Update the dataframe
-    #df['age'] += 1
-    st.warning("Dataframe is changed")
+    df['age'] += 1
 
 # Display the data editor
-edited_df = st.data_editor(df, on_change=on_change, num_rows="dynamic")
+st.data_editor(df, on_change=on_change)
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
-
-st.code(st.session_state)
+# Display the updated dataframe
+st.write(df)
