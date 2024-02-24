898e4a6550092273fec463881653c63479391bab
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-24 14:11:51 -0800

    Update data-editor.py

--- data-editor.py
+++ data-editor.py
@@ -10,6 +10,5 @@ df = pd.DataFrame(
 )
 edited_df = st.data_editor(df)
 
-#favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-favorite_command = edited_df.loc[edited_df["rating"]]
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
