7f4642a8f9b56b6bf83cd34fb2b72353e8ab9743
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-25 14:27:15 -0800

    Update data-editor.py

--- data-editor.py
+++ data-editor.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 from vega_datasets import local_data as ld
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
+
 
 @st.cache_data
 def get_data(id: int):
@@ -14,10 +15,15 @@ def highlight_changes(val):
     background = f"background-color:lightgray;" if val else ""
     return f"{color} {background}"
 
+st.subheader("Edit your data ⬇️")
+data = get_data(1)
+editor_df = st.data_editor(
+                data, key="airport_edit", 
+                num_rows="dynamic", 
+                use_container_width=True )
 
-def show_diff(
-    source_df: pd.DataFrame, modified_df: pd.DataFrame, editor_key: dict
-) -> None:
+
+def show_diff(source_df: pd.DataFrame, modified_df: pd.DataFrame, editor_key: dict) -> None:
     target = pd.DataFrame(editor_key.get("edited_rows")).transpose().reset_index()
     modified_columns = [i for i in target.notna().columns if i != "index"]
     source = source_df.iloc[target.index].reset_index()
@@ -30,7 +36,9 @@ def show_diff(
         on="index",
         suffixes=["_BEFORE", "_AFTER"],
     )
+    
     after_columns = [i for i in changes.columns if "_AFTER" in i]
+    
     for cl in changes:
         if cl in after_columns:
             new_col = cl.replace("_AFTER", "_BEFORE")
@@ -40,11 +48,13 @@ def show_diff(
     st.caption("Showing only modified columns")
 
     change_markers = changes.copy()
+    
     for cl in change_markers:
         if cl in after_columns:
             new_col = cl.replace("_AFTER", "_BEFORE")
             change_markers[cl] = change_markers[cl] != change_markers[new_col]
             change_markers[new_col] = change_markers[cl]
+            
     st.dataframe(
         changes.style.apply(
             lambda _: change_markers.applymap(highlight_changes), axis=None
@@ -64,9 +74,3 @@ show_diff(
     source_df=data, modified_df=editor_df, editor_key=st.session_state["airport_edit"]
 )
 
-
-st.subheader("Edit your data ⬇️")
-data = get_data(1)
-editor_df = st.data_editor(
-    data, key="airport_edit", num_rows="dynamic", use_container_width=True
-)
