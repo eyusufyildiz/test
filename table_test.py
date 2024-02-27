8b01bb024e67964d31d3d855dc0e20771aba6f52
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-27 01:13:31 -0800

    Update table_test.py

--- table_test.py
+++ table_test.py
@@ -4,19 +4,15 @@ import streamlit as st
 data_df = pd.DataFrame(
     {
         "sales": [200, 550, 1000, 80],
+        "stock": [20, 55, 67, 80],
     }
 )
 
 st.data_editor(
     data_df,
     column_config={
-        "sales": st.column_config.ProgressColumn(
-            "Sales volume",
-            help="The sales volume in USD",
-            format="$%f",
-            min_value=0,
-            max_value=1000,
-        ),
+        "sales": st.column_config.ProgressColumn("Sales volume", help="The sales volume in USD", format="$%f", min_value=0, max_value=1000),
+        "stock": st.column_config.ProgressColumn("stock volume", help="TNum of stock", format="$%f", min_value=0, max_value=100),
     },
     hide_index=True,
 )
