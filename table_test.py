e56ee0bc838f9ed71bef5c059b8ac9d2ab2ccab4
eyusufyildiz <eyusufyildiz@gmail.com>
2024-02-27 01:08:06 -0800

    Update table_test.py

--- table_test.py
+++ table_test.py
@@ -1,26 +1,22 @@
-
-import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid
+import streamlit as st
 
-df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+data_df = pd.DataFrame(
+    {
+        "sales": [200, 550, 1000, 80],
+    }
+)
 
-grid_options = {
-    "columnDefs": [
-        {
-            "headerName": "col1",
-            "field": "col1",
-            "editable": True,
-        },
-        {
-            "headerName": "col2",
-            "field": "col2",
-            "editable": False,
-        },
-    ],
-}
-
-grid_return = AgGrid(df, grid_options)
-new_df = grid_return["data"]
-
-#st.write(new_df)
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.ProgressColumn(
+            "Sales volume",
+            help="The sales volume in USD",
+            format="$%f",
+            min_value=0,
+            max_value=1000,
+        ),
+    },
+    hide_index=True,
+)
