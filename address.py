7414fa2655fe6d399102059238a0e85f4b6a49f2
eyusufyildiz <eyusufyildiz@gmail.com>
2024-03-02 18:00:40 -0800

    Update address.py

--- address.py
+++ address.py
@@ -70,7 +70,8 @@ def vpn_server_request():
         
                 if st.button("Send"):
                     st.code(location_info)
-                    vpn_survey_info = ", ".join( location_info )
+                    #vpn_survey_info = ", ".join( location_info )
+                    vpn_survey_info = ", ".join( lc for lc in location_info if ls )
                     st.write(vpn_survey_info)
     except Exception as e:
         st.text(f"Error3: {str(e)}")
