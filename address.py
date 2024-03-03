5b4d6d156c952c4feff0abeac732d19c1e9192ea
eyusufyildiz <eyusufyildiz@gmail.com>
2024-03-02 17:02:19 -0800

    Update address.py

--- address.py
+++ address.py
@@ -83,12 +83,12 @@ def vpn_server_request():
 #######################
 
 def c1():
-    tab1, tab2 = st.tabs(["Send your Comments...", 
-                          "Where would do you want a VPN server?"])
+    tab1, tab2 = st.tabs([ "Where would do you want a VPN server?", "Send your Comments..."])
     with tab1:
         folium_map()
-    with tab2:
         st.write("Click on map for where you want a VPN server. Or, fill up form and send after click. We will add it to our VPN server lists asap.")
+
+    with tab2:
         folium_map()
         vpn_server_request()
 
