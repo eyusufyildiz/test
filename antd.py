befff97001f99882f04b24e7a23d0406194e3e00
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-07 07:29:33 +0000

    UU

--- antd.py
+++ antd.py
@@ -4,6 +4,7 @@ import streamlit_antd_components as sac
 
 import streamlit as st
 import streamlit_antd_components as sac
+import time 
 
 btn = sac.buttons(
     items=['button1', 'button2', 'button3'],
@@ -19,6 +20,7 @@ st.write(f'The selected button label is: {btn}')
 
 if sac.buttons( items=["btn1"]):
     print("BTN1")
+    time.sleep(3)
 
 # 
 # sac.menu(items=[
