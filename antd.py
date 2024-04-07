6b0c16637e2f1b7d05f9be43495fa063618c234e
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-07 05:04:14 +0000

    UU

--- antd.py
+++ antd.py
@@ -1,9 +1,11 @@
 import streamlit_antd_components as antd
 
-antd.Button(label="Click me")
 
-antd.Menu(items=[
+antd.button(label="Click me")
+
+antd.menu(items=[
     antd.MenuItem(label="Home"),
     antd.MenuItem(label="About"),
     antd.MenuItem(label="Contact"),
 ])
+
