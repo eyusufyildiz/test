4471de98a23af0b573dc0b23cfa013d82f594515
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-07 07:24:39 +0000

    UU

--- antd.py
+++ antd.py
@@ -17,7 +17,7 @@ btn = sac.buttons(
 st.write(f'The selected button label is: {btn}')
 
 
-sac.buttons(label="Click me")
+a=sac.buttons(label="Click me")
 # 
 # sac.menu(items=[
 #     sac.MenuItem(label="Home"),
