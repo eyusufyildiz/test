e4719f3fa1e53f32d8a8cc24c3b690e80c29508a
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-07 07:36:00 +0000

    UU

--- antd.py
+++ antd.py
@@ -18,11 +18,14 @@ if sac.buttons( items=["btn1"]):
     print("BTN1")
 
 
-with st.sidebar:
-    opt= sac.menu(items=[
-            sac.MenuItem(label="Home"),
-            sac.MenuItem(label="About"),
-            sac.MenuItem(label="Contact"),
-         ])
-    
-    print(opt)
+from streamlit_antd_components import menu, MenuItem
+
+item = menu([
+    MenuItem('menu1', icon='house'),
+    MenuItem('menu2', icon='app', children=[
+        MenuItem('menu3', icon='twitter'),
+        MenuItem('disabled', disabled=True),
+    ]),
+], format_func='title', open_all=True)
+st.write(f'The selected menu label is: {item}')
+
