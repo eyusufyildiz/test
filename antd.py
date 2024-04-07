3598d49cbf4ed51ac4a9a32adcd997f859bd5e6c
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-07 05:09:05 +0000

    UU

--- antd.py
+++ antd.py
@@ -2,11 +2,26 @@ import streamlit as st
 import streamlit_antd_components as sac
 
 
-sac.button(label="Click me")
+import streamlit as st
+import streamlit_antd_components as sac
 
-sac.menu(items=[
-    sac.MenuItem(label="Home"),
-    sac.MenuItem(label="About"),
-    sac.MenuItem(label="Contact"),
-])
+btn = sac.buttons(
+    items=['button1', 'button2', 'button3'],
+    index=0,
+    format_func='title',
+    align='center',
+    direction='horizontal',
+    radius='lg',
+    return_index=False,
+)
+st.write(f'The selected button label is: {btn}')
+
+
+# sac.button(label="Click me")
+# 
+# sac.menu(items=[
+#     sac.MenuItem(label="Home"),
+#     sac.MenuItem(label="About"),
+#     sac.MenuItem(label="Contact"),
+# ])
 
