8a884351c55c2b960e551b670c63f7cd47778dc0
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-06 21:36:25 -0700

    Create antd.py

---
+++ antd.py
@@ -0,0 +1,9 @@
+import streamlit_antd_components as antd
+
+antd.Button(label="Click me")
+
+antd.Menu(items=[
+    antd.MenuItem(label="Home"),
+    antd.MenuItem(label="About"),
+    antd.MenuItem(label="Contact"),
+])
